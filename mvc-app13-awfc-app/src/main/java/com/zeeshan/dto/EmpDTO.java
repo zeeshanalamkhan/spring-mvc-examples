b02c0308d7d41aa41aa936ebf77441613d046c6e
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-03-25 23:50:36 +0530

    improved DTO class

--- mvc-app13-awfc-app/src/main/java/com/zeeshan/dto/EmpDTO.java
+++ mvc-app13-awfc-app/src/main/java/com/zeeshan/dto/EmpDTO.java
@@ -1,5 +1,7 @@
 package com.zeeshan.dto;
 
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +14,9 @@ public class EmpDTO {
 	private Integer empNo;
 	private String ename;
 	private String job;
+	private Integer mgr;
+	private Date hireDate;
 	private Integer salary;
+	private Integer comm;
+	private Integer deptNo;
 }
