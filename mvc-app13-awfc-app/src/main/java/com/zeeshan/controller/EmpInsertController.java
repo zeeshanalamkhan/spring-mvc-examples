4542878d15c36628a2cd92e3977d761843b2d451
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-03-25 23:48:45 +0530

    insert controller with initBinder(-) added

--- mvc-app13-awfc-app/src/main/java/com/zeeshan/controller/EmpInsertController.java
+++ mvc-app13-awfc-app/src/main/java/com/zeeshan/controller/EmpInsertController.java
@@ -1,9 +1,15 @@
 package com.zeeshan.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractWizardFormController;
 
@@ -23,7 +29,8 @@ public class EmpInsertController extends AbstractWizardFormController {
 	protected ModelAndView processFinish(HttpServletRequest request, HttpServletResponse response, Object command,
 			BindException errors) throws Exception {
 		EmpCommand cmd = (EmpCommand) command;
-		EmpDTO dto = new EmpDTO(cmd.getEmpNo(), cmd.getEname(), cmd.getJob(), cmd.getSalary());
+		EmpDTO dto = new EmpDTO(cmd.getEmpNo(), cmd.getEname(), cmd.getJob(), cmd.getMgr(), cmd.getHireDate(),
+				cmd.getSalary(), cmd.getComm(), cmd.getDeptNo());
 		String res = service.registerEmp(dto);
 
 		return new ModelAndView("result", "result", res);
@@ -35,7 +42,15 @@ public class EmpInsertController extends AbstractWizardFormController {
 
 		return new ModelAndView("home");
 	}
-	
+
+	@Override
+	public void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(sdf, true));
+
+	}
+
 	@Override
 	protected ModelAndView handleInvalidSubmit(HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
