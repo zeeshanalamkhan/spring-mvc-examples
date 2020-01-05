668a5a7759cee400b0163cc0ab339053dd4c1b2a
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-01-05 15:49:20 +0530

    db sql file

---
+++ book-api/src/main/resources/db.sql
@@ -0,0 +1,4 @@
+insert into book values(101,'Y Kanetkar','Let Us C');
+insert into book values(102,'Y Kanetkar','Pointers In C');
+insert into book values(103,'James Benjamin','Data Structures');
+insert into book values(104,'Sulekha Devi','My Trick');
